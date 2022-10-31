859fedb7bc82b13a7597e2d375b2271bd7ee0061
jsalgua027 <jsalcedoguarde@gmail.com>
2022-10-31 14:44:30 +0100

    ejercicio a medio terminar, faltan dos metodos

--- Utilidades/src/main/java/com/mycompany/utilidades/Utilidades.java
+++ Utilidades/src/main/java/com/mycompany/utilidades/Utilidades.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.utilidades;
 
 import java.util.Random;
@@ -14,31 +13,68 @@ import java.util.Scanner;
  */
 public class Utilidades {
 
-    private static Scanner  teclado= new Scanner(System.in);
-    
+    private static Scanner teclado = new Scanner(System.in);
+
     private static Random aleatorio = new Random();
-    
-    
+
     // leer enteros sin errores 
-    
-    public static int leerEnteroSinErrores(){
-        int num=0;
-        
-        try {
+    public static int leerEnteroSinErrores() {
+        int num = 0;
+        boolean repetir = true;
+
+        do {
             System.out.println("Introduce el numero entero");
+            try {
+
+                num = teclado.nextInt();
+                repetir = false;
+
+            } catch (NumberFormatException nfe) {
+                System.out.println("No has introducido un numero entero");
+
+            }
+
+        } while (repetir);
+
+        return num;
+    }
+    
+    //pedir un numero entre un rango bucle hasta que este en el rango
+    
+    public static int leerNumeroRango(){
+    numero=0;
+
+   return numero;
+}
+    
+    
+    
+    
+    
+
+    
+    // numero entero dentro de un rango 
+    
+    public static boolean numeroDentroRango(int num, int min, int max){
+    boolean rango = true;
+    int numero= leerEnteroSinErrores();
             
-            
-        } catch (Exception e) {
+        if (numero>=min && numero <= max){
+            System.out.println(" El numero esta dentro del rango");
+        
+        }else {
+            System.out.println("El numero esta fuera de rango");
+            rango=false;
         }
-        
-        
     
     
+    
+    
+    return rango;
     }
     
     
     
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    
+    
 }
